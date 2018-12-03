09c5cc8733ecee8c69f8901ba3dfadeb3e3f2785
Li Xinxiao <wood@Woods-MacBook.local>
2018-12-03 15:39:55 +0800

    update code

--- RunbookTest.ps1
+++ RunbookTest.ps1
@@ -5,9 +5,27 @@ $webapp_templateObject = Invoke-WebRequest -uri $webapp_templateUrl| ConvertFrom
 $linked_templateObject = Invoke-WebRequest -uri $linked_templateUrl| ConvertFrom-Json
 
 $linked_templateObject.properties.templateLink.uri = "https://raw.githubusercontent.com/lesintion/AzureARMtemplate/master/arm_applicationinsight_template.json"
-$linked_parameters = @{name="TestAppinsightWood"; location="Central US"}
+$linked_parameters = @{
+    name     = "TestAppinsightWood"
+    location = "Central US"
+}
 $linked_templateObject.properties.parameters = $linked_parameters
 
-$webapp_templateObject.resources.Add($linked_templateObject)
+
+
+$webapp_templateObject.resources += $linked_templateObject
+
+$webapp_parameters = @{
+    name                    = "automationtestwlazure11"
+    hostingPlanName         = "ServicePlanautomationTest_bbbb"
+    hostingEnvironment      = ""
+    location                = "Central US"
+    sku                     = "Standard"
+    skuCode                 = "S1"
+    workerSize              = "0"
+    serverFarmResourceGroup = "eatai"
+    subscriptionId          = "0d067ba0-399d-435e-98bd-a33ba0d5fbcd"
+}
+
 
 Write-Output $templateObject
