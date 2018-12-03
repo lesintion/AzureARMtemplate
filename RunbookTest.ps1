825deb823098a47d7d76857686e1ad6c12c3c2df
Li Xinxiao <wood@Woods-MacBook.local>
2018-12-03 17:22:37 +0800

    update code

--- RunbookTest.ps1
+++ RunbookTest.ps1
@@ -1,4 +1,5 @@
 $webapp_templateUrl = "https://raw.githubusercontent.com/lesintion/AzureARMtemplate/master/arm_standard_webapp.json"
+$applicationInsight_templateUrl = "https://raw.githubusercontent.com/lesintion/AzureARMtemplate/master/arm_applicationinsight_template.json"
 $linked_templateUrl = "https://raw.githubusercontent.com/lesintion/AzureARMtemplate/master/arm_linked_template.json"
 $webapp_deployment_templateUrl = "https://raw.githubusercontent.com/lesintion/AzureARMtemplate/master/arm_webapp_deployment_template.json"
 
@@ -10,30 +11,47 @@ Function RegisterRP {
     Write-Output "Registering resource provider '$ResourceProviderNamespace'";
     Register-AzureRmResourceProvider -ProviderNamespace $ResourceProviderNamespace;
 }
-
-$webapp_parameters = @{
-    name                      = "automationtestwlazure11"
-    hostingPlanName           = "ServicePlanautomationTest_bbbb"
-    hostingEnvironment        = ""
-    location                  = "East US"
-    sku                       = "Standard"
-    skuCode                   = "S1"
-    workerSize                = "0"
-    serverFarmResourceGroup   = "k8s"
-    subscriptionId            = "0d067ba0-399d-435e-98bd-a33ba0d5fbcd"
-    siteConfig                = @{}
-    includeApplicationInsight = $false
+function TestWebApp {
+    param (
+        $resourceGroupName = "k8s",
+        $templateUrl = $webapp_templateUrl 
+    )
+    $webapp_parameters = @{
+        name                      = "automationtestwlazure11"
+        hostingPlanName           = "ServicePlanautomationTest_bbbb"
+        hostingEnvironment        = ""
+        location                  = "East US"
+        sku                       = "Standard"
+        skuCode                   = "S1"
+        workerSize                = "0"
+        serverFarmResourceGroup   = "k8s"
+        subscriptionId            = "0d067ba0-399d-435e-98bd-a33ba0d5fbcd"
+        siteConfig                = @{}
+        includeApplicationInsight = $false
+    }
+    
+    if ($webapp_parameters.includeApplicationInsight) {
+        RegisterRP "microsoft.insights"
+    }
+    
+    New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -NameFromTemplate $webapp_parameters.name -TemplateUri $templateUrl -TemplateParameterObject $webapp_parameters
 }
 
-# $webapp_template = Invoke-WebRequest -uri $webapp_deployment_templateUrl| ConvertFrom-Json
-# $webapp_template.properties.template = $webapp_templateObject
-# $webapp_template.properties.parameters = $webapp_parameters
-$resourceGroupName = "k8s"
+function TestApplicationInsight {
+    param (
+        $resourceGroupName = "k8s",
+        $templateUrl = $applicationInsight_templateUrl 
+    )
 
-if ($webapp_parameters.includeApplicationInsight) {
-    RegisterRP "microsoft.insights"
+    $appInsight_parameters = @{
+        name="woodtest111"
+    }
+
+    $key = New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -NameFromTemplate $appInsight_parameters.name -TemplateUri $templateUrl -TemplateParameterObject $appInsight_parameters
+
+    Write-Output $key 
+    
 }
 
-New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -NameFromTemplate $webapp_parameters.name -TemplateUri $webapp_templateUrl -TemplateParameterObject $webapp_parameters;
+TestApplicationInsight
 
-Write-Output $templateObject 
