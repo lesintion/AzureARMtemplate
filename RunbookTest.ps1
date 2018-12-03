38103452f325be98d5494fffb64556ed4036f76c
Li Xinxiao <wood@Woods-MacBook.local>
2018-12-03 15:15:26 +0800

    update code

--- RunbookTest.ps1
+++ RunbookTest.ps1
@@ -1,3 +1,13 @@
-$templateUrl = "https://raw.githubusercontent.com/lesintion/AzureARMtemplate/master/arm_webapp_template.json";
-$templateObject = Invoke-WebRequest -uri $templateUrl| ConvertFrom-Json
+$webapp_templateUrl = "https://raw.githubusercontent.com/lesintion/AzureARMtemplate/master/arm_standard_webapp.json"
+$linked_templateUrl = "https://raw.githubusercontent.com/lesintion/AzureARMtemplate/master/arm_linked_template.json"
+
+$webapp_templateObject = Invoke-WebRequest -uri $webapp_templateUrl| ConvertFrom-Json
+$linked_templateObject = Invoke-WebRequest -uri $linked_templateUrl| ConvertFrom-Json
+
+$linked_templateObject.properties.templateLink.uri = "https://raw.githubusercontent.com/lesintion/AzureARMtemplate/master/arm_applicationinsight_template.json"
+$linked_parameters = @{name="TestAppinsightWood"; location="Central US"}
+$linked_templateObject.properties.parameters = $linked_parameters
+
+$webapp_templateObject.resources.Add($linked_templateObject)
+
 Write-Output $templateObject
